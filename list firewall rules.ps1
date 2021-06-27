4b7dcdbb65311baafd5cd82c740d77fab606331b
Ali Rabiei <68955283+ali-rabiei1989@users.noreply.github.com>
2021-06-27 09:22:34 +0430

    Initial commit

---
+++ list firewall rules.ps1
@@ -0,0 +1,19 @@
+# Author : Ali Rabiei
+# Creation Date : 11/18/2020
+# This script list all enabled firewall rules
+# --------------------------------------
+
+Import-Module NetSecurity
+
+$rules = Get-NetFirewallRule -Enabled True -direction Inbound
+
+foreach ($rule in $rules){
+
+    Get-NetFirewallPortFilter | ForEach-Object {
+        [pscustomobject]@{
+            Rule = $Rule.DisplayName
+            Protocol = $_.Protocol
+            LocalPort = $_.LocalPort
+        }
+    }
+}
